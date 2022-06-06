937977f3ce317893db256c40cf7c05380d9e8a77
Mayank9664 <sureliyamayank@gmail.com>
2022-06-06 10:54:40 +0530

    Update Model Page View File

--- lib/models/model_page_view.dart
+++ lib/models/model_page_view.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class ModelPageView {
   String? title;
   String? subTitle;
@@ -7,4 +9,20 @@ class ModelPageView {
     this.subTitle,
     this.image,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'subTitle': subTitle,
+      'image': image,
+    };
+  }
+
+  factory ModelPageView.fromMap(Map<String, dynamic> map) {
+    return ModelPageView(
+      title: map['title'],
+      subTitle: map['subTitle'],
+      image: map['image'],
+    );
+  }
 }
